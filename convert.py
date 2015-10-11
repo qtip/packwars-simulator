c1a02f4457ed3a947251f7b98f1a0be46a897d23
Daniel Snider <qtipkk@gmail.com>
2015-10-11 14:32:16 -0700

    Use booster formula to generate boosters
    - Allow the Innistrad's double faced cards
    - Remove sets like timespiral

--- convert.py
+++ convert.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python3
 
+from collections import Counter
+
+KINDS = (
+    "common",
+    "uncommon",
+    "rare",
+    "rare/mythic rare",
+    "land",
+    "double faced",
+)
+
+class InvalidSet(Exception):
+    pass
+
 def make_name(block, name):
     """
     >>> make_name("Shadowmoor", "Eventide")
@@ -11,29 +25,73 @@ def make_name(block, name):
         return name
     return "{block} - {name}".format(block=block, name=name)
 
+def generate_card_lists(kind, data):
+    """
+    >>> generate_card_lists("rare/mythic rare", mtgjson_set)
+    {"rare": ["rare1", ... ], "mythic rare": ["mythic1", ...]}
+    >>> generate_card_lists("uncommon", mtgjson_set)
+    {"uncommon": ["uncommon1", ... ]}
+    """
+
+    out = dict()
+    if kind == "common":
+        out[kind] = [c['name'] for c in data if c['rarity'] == "Common"]
+    elif kind == "uncommon":
+        out[kind] = [c['name'] for c in data if c['rarity'] == "Uncommon"]
+    elif kind == "rare":
+        out[kind] = [c['name'] for c in data if c['rarity'] == "Rare"]
+    elif kind == "rare/mythic rare":
+        out["rare"] = [c['name'] for c in data if c['rarity'] == "Rare"]
+        out["mythic rare"] = [c['name'] for c in data if c['rarity'] == "Mythic Rare"]
+    elif kind == "double faced":
+        out[kind] = [c['name'] for c in data if c['layout'] == "double-faced" and 'manaCost' in c]
+    elif kind == "land":
+        out[kind] = ["Plains", "Island", "Swamp", "Mountain", "Forest"]
+    else:
+        out[kind] = []
+    return out
+
+
+def convert_booster(data):
+    """
+    >>> convert_booster(mtgjson_set)
+    {'common': 10, 'land': 1, 'rare/mythic rare': 1, 'uncommon': 3}
+
+    Will throw an exception if the booster is a format that the simulator
+    won't understand, e.g. Future Sight
+    """
+    counter = Counter()
+    junk = ("marketing", "checklist")
+    for kinds_of_card in data:
+        if isinstance(kinds_of_card, str):
+            # make is so that we're always dealing with an array
+            kinds_of_card = [kinds_of_card]
+        junkless = [k for k in kinds_of_card if k not in junk]
+        if not junkless:
+            # ignore all junk
+            continue
+        kind = '/'.join(junkless)
+        counter.update([kind])
+    if any(kind not in KINDS for kind in counter):
+        return dict()
+    return dict(counter)
+
 def convert_set(data):
-    mythics = list({card['name'] for card in data['cards'] if card['rarity'] == "Mythic Rare"})
-    rares = list({card['name'] for card in data['cards'] if card['rarity'] == "Rare"})
-    uncommons = list({card['name'] for card in data['cards'] if card['rarity'] == "Uncommon"})
-    commons = list({card['name'] for card in data['cards'] if card['rarity'] == "Common"})
-    mythics.sort()
-    rares.sort()
-    uncommons.sort()
-    commons.sort()
+    booster = convert_booster(data['booster'])
     out = {
         "name": make_name(data.get('block'), data['name']),
-        "mythics": mythics,
-        "rares": rares,
-        "uncommons": uncommons,
-        "commons": commons,
+        "booster": booster,
     }
+    for kind in booster:
+        out.update(generate_card_lists(kind, data['cards']))
     return out
 
 def convert(data):
     types = ('expansion', 'core', 'un')
     sets = [set for set in data.values() if set['type'] in types and 'booster' in set]
     sets.sort(key=lambda set: set.get('releaseDate', 0))
-    return [convert_set(set) for set in sets[::-1]]
+    converted = (convert_set(set) for set in sets[::-1])
+    return [set for set in converted if set['booster']]
 
 if __name__ == "__main__":
     import sys
--- packwars.js
+++ packwars.js
@@ -1,26 +1,24 @@
-var LANDS = ["Plains", "Island", "Swamp", "Mountain", "Forest"];
 
 var PackWarsSimulator = function(sets) {
     this.sets = sets;
 }
 
-PackWarsSimulator.prototype.makeDeck = function(packs) {
-    var cards = Array.prototype.concat.apply([], packs);
-    for (i = 0; i < packs.length; i++) {
-        cards = cards.concat(LANDS, LANDS);
-    }
-    cards.sort();
+PackWarsSimulator.LANDS = ["Plains", "Island", "Swamp", "Mountain", "Forest"];
+
+PackWarsSimulator.arrayToDec = function(array) {
+    array.sort();
 
     var out = [];
     var current = null;
     var idx = 0;
-    for (i = 0; i < cards.length; i++) {
-        if(cards[i] != current) {
+    var i;
+    for (i = 0; i < array.length; i++) {
+        if(array[i] != current) {
             // new card!
             if (current != null) {
                 out.push(i-idx + " " + current);
             }
-            current = cards[i];
+            current = array[i];
             idx = i;
         }
     }
@@ -31,31 +29,66 @@ PackWarsSimulator.prototype.makeDeck = function(packs) {
     return out.join("\n");
 }
 
-var takeOne = function(array) {
-    return array[Math.floor(Math.random()*array.length)];
+PackWarsSimulator.landsForCards = function(cardCount) {
+    out = []
+    var amount;
+    if (cardCount == 15) {
+        amount = 3;
+    } else {
+        amount = Math.ceil(cardCount*2/15);
+    }
+    var i;
+    for (i = 0; i < amount; i++) {
+        out = out.concat(PackWarsSimulator.LANDS);
+    }
+    return out;
 }
 
-PackWarsSimulator.prototype.makePack = function(setIdx) {
-    var set = this.sets[setIdx];
-    var pack = [];
-    for (i = 0; i < 10; i++) {
-        pack.push(takeOne(set.commons));
+PackWarsSimulator.prototype.makeDeck = function(idxs) {
+    var cards = [];
+    var i;
+    for (i = 0; i < idxs.length; i++) {
+        cards = cards.concat(this.makeBooster(idxs[i]));
     }
-    for (i = 0; i < 3; i++) {
-        pack.push(takeOne(set.uncommons));
-    }
-    if (set.mythics.length == 0 || Math.random() > 1/8) {
-        pack.push(takeOne(set.rares));
-    } else {
-        pack.push(takeOne(set.mythics));
-    }
-    pack.push(takeOne(LANDS));
-    return pack;
+    cards = cards.concat(PackWarsSimulator.landsForCards(cards.length));
+    return PackWarsSimulator.arrayToDec(cards);
+}
 
+PackWarsSimulator.sample = function(array, n) {
+    out = [];
+    var i;
+    for (i = 0; i < n; i++) {
+        var idx = Math.floor(Math.random() * (array.length-i)) + i;
+        var tmp = array[idx];
+        array[idx] = array[i];
+        array[i] = tmp;
+        out.push(tmp);
+    }
+    return out;
+}
+
+PackWarsSimulator.prototype.makeBooster = function(setIdx) {
+    var sample = PackWarsSimulator.sample;
+    var set = this.sets[setIdx];
+    var booster = [];
+    for (kind in set.booster) {
+        var count = set.booster[kind]
+        if (kind == "rare/mythic rare") {
+            if (Math.random() > 1/8) {
+                booster = booster.concat(sample(set['rare'], count));
+            } else {
+                booster = booster.concat(sample(set['mythic rare'], count));
+            }
+        } else {
+            booster = booster.concat(sample(set[kind], count));
+        }
+    }
+    return booster;
 }
 
 PackWarsSimulator.prototype.setNames = function() {
     var out = [];
+    var i;
     for (i = 0; i < this.sets.length; i++) {
         out.push({"key": "" + i, "name": this.sets[i].name});
     }
